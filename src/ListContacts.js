2423835b3d21ca2b686c06070a86497c2a71ab33
evwilkin <evan.wilkinson@gmail.com>
2017-12-23 18:08:42 -0500

    Adding React Router components

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -50,7 +50,12 @@ class ListContacts extends Component {
             placeholder='Search Contacts'
             value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
-           />
+          />
+          <a
+            href='#create'
+            onClick={this.props.onNavigate}
+            className='add-contact'
+          >Add Contact</a>
         </div>
 
         {showingContacts.length !== contacts.length && (
